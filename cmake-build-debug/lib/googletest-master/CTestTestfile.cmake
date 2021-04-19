6ff9cbad81732efe18c60cf387f7387493cea76a
ziolko <aaartiii99@gmail.com>
2021-04-20 00:04:30 +0200

    delete ignored folder

--- cmake-build-debug/lib/googletest-master/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/artur/cpp_projekty/rotation3D/lib/googletest-master
-# Build directory: /home/artur/cpp_projekty/rotation3D/cmake-build-debug/lib/googletest-master
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- cmake-build-debug/lib/googletest-master/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/artur/cpp_projekty/rotation3D/lib/googletest-master/googletest
-# Build directory: /home/artur/cpp_projekty/rotation3D/cmake-build-debug/lib/googletest-master/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
