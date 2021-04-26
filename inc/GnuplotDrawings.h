041eef7e50b76952936c0003cf3ea4cbb177575b
ziolko <aaartiii99@gmail.com>
2021-04-26 20:14:29 +0200

    test new gnuplot-conector library

--- inc/GnuplotDrawings.h
+++ inc/GnuplotDrawings.h
@@ -9,20 +9,20 @@
 #include <lacze_do_gnuplota.hh>
 #include <fstream>
 
-//class GnuplotDrawings{
-//private:
-//    std::string fileName;
-//    PzG::LaczeDoGNUPlota GNU;
-//    double XRange[2];
-//    double YRange[2];
-//
-//public:
-//    GnuplotDrawings();
-//    GnuplotDrawings(std::string _fileName, double _XRange[2], double _YRange[2]);
-//    void drawRectangle(Cuboid &Rec);
-//    void drawVector(Vector &Vec);
-//    void animateRotateRectangle(Cuboid &Rec, double &degree);
-//    void animateTranslateRectangle(Cuboid &Rec, Vector &translation);
-//};
+class GnuplotDrawings{
+private:
+    std::string fileName;
+    PzG::LaczeDoGNUPlota GNU;
+    double XRange[2];
+    double YRange[2];
+
+public:
+    GnuplotDrawings();
+    GnuplotDrawings(std::string _fileName, double _XRange[2], double _YRange[2]);
+    void drawRectangle(Cuboid &Rec);
+    void drawVector(Vector &Vec);
+    void animateRotateRectangle(Cuboid &Rec, double &degree);
+    void animateTranslateRectangle(Cuboid &Rec, Vector &translation);
+};
 
 #endif //ROTATION3D_GNUPLOTDRAWINGS_H
